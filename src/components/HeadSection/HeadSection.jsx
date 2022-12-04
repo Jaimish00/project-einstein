7c1be6248d072909f27428c1ea21221f0ffd0156
Jaimish Trivedi <jaimishtrivedi00@gmail.com>
2022-12-04 12:29:07 +0530

    Add birthday music

--- src/components/HeadSection/HeadSection.jsx
+++ src/components/HeadSection/HeadSection.jsx
@@ -3,9 +3,21 @@ import "./HeadSection.scss";
 import Confetti from "react-confetti";
 import { Fade } from "react-awesome-reveal";
 import useWindowSize from "react-use/lib/useWindowSize";
+import useSound from "use-sound";
+
+import happyBirthdayMusic from "../../sounds/happy-birthday-music-box.wav";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const HeadSection = () => {
 	const { width, height } = useWindowSize();
+	const [play] = useSound(happyBirthdayMusic);
+
+	useEffect(() => {
+		setTimeout(() => {
+			play();
+		}, 5);
+	}, [play]);
 
 	return (
 		<section className="section head-section">
